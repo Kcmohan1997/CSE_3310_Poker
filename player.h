b64b546951f7f9676926c398088899c3a665df4a
Kcmohan1997 <54957060+Kcmohan1997@users.noreply.github.com>
2020-03-20 01:09:12 -0500

    Add files via upload

---
+++ player.h
@@ -0,0 +1,32 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+// #include <gtkmm.h>
+
+using namespace std;
+
+class Player
+{
+  public:
+    int Player;
+    int money;
+    Hand Player_Hand;
+    int Get_Money();
+    int Add_To_Money(int money);
+    Hand Set_Hand(Hand);
+    Hand Get_Hand(Hand);
+    bool Check_Swap();
+    bool Swap_Cards(Hand Player_Hand);
+    int Check(int money);
+    int Bet(int money);
+    void Fold(Hand);
+    bool Check_Hand(Hand);
+    int Set_Player_Id();
+
+
+  private:
+    bool Swap_Allowed;
+    bool Amount_Of_Cards;
+}
+
+#endif
